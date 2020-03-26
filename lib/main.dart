161881803ed6114d39a85d628c7d6e126e83e3c6
Trent Bowden <trentb.business@gmail.com>
2020-03-26 17:26:11 +1030

    uploading files

---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Material(
+      child:
+
+      Container(
+        width: double.infinity,
+        color: Colors.teal,
+        child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/myPicture.jpg'),
+                  radius: 50,
+                  backgroundColor: Colors.grey,
+                ),
+                Text(
+                  "Trent Bowden",
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 25,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                    "FLUTTER DEVELOPER",
+                    style: TextStyle(
+                      fontFamily: "SourceSansPro",
+                      color: Colors.teal.shade100,
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    )
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 25.0
+                    ),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        "0412 345 678",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "SourceSansPro",
+                          color: Colors.teal.shade900,
+                        ),
+                      ),
+                    )
+                ),
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 25.0
+                    ),
+                    child: ListTile(
+                      leading: Icon(
+                          Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        "trentb.business@gmail.com",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "SourceSansPro",
+                          color: Colors.teal.shade900,
+                        ),
+                      ),
+                    )
+                )
+              ],
+            )
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
